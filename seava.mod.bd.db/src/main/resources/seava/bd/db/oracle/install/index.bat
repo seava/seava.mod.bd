c986640c448db1d394aca8e013e84a3e770e1110
Attila Mathe <attila.mathe@dnet-ebusiness-suite.com>
2014-04-28 17:20:24 +0300

    DB scripts moved to folders according to module package

---
+++ seava.mod.bd.db/src/main/resources/seava/bd/db/oracle/install/index.bat
@@ -0,0 +1,3 @@
+@echo off
+call ../../../../../_lib/liquibase/liquibase --defaultsFile=../../../../../_lib/mysql.properties --classpath=../../../../../_lib/jdbc-mysql.jar --changeLogFile=install.xml update
+pause...
